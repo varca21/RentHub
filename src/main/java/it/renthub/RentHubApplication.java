4e48bee9a64a6de19d408d325501329e96b1a413
varca21 <varcasianicola@libero.it>
2022-02-28 12:07:11 +0100

    creazione progetto

---
+++ src/main/java/it/renthub/RentHubApplication.java
@@ -0,0 +1,13 @@
+package it.renthub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/renthub/RentHubApplicationTests.java
@@ -0,0 +1,13 @@
+package it.renthub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
