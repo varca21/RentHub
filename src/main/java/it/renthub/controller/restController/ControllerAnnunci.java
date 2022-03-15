8ed9d2fa83ee378c65fbc1cfd9afcfe4cccf3c35
FrancescoC1998 <curcio.francesco98@gmail.com>
2022-03-15 22:31:24 +0100

    implementata classe controllerAnnunci

---
+++ src/main/java/it/renthub/controller/restController/ControllerAnnunci.java
@@ -0,0 +1,7 @@
+package it.renthub.controller.restController;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/rest/annunci")
+public class ControllerAnnunci {
+}
