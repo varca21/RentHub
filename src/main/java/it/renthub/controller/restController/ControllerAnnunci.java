5981c853a06ab5c82216e0b4ec85b963f2699984
varca21 <varcasianicola@libero.it>
2022-03-15 22:53:03 +0100

    aggiunto controller tipologia annunci

--- src/main/java/it/renthub/controller/restController/ControllerAnnunci.java
+++ src/main/java/it/renthub/controller/restController/ControllerAnnunci.java
@@ -1,7 +1,21 @@
 package it.renthub.controller.restController;
 
+import it.renthub.model.bean.Tipologia;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@RestController
 @RequestMapping("/rest/annunci")
 public class ControllerAnnunci {
+
+    @GetMapping("/tipologie")
+    List<String> tipologieAnnunci() {
+        return Stream.of(Tipologia.values()).map(Tipologia::name).collect(Collectors.toList());
+    }
 }
